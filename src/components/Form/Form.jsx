c320cebaa83801295f49ff06df556a9ec4ca7a76
Ben Capp <bentucker333@gmail.com>
2023-02-25 18:39:28 -0600

    setup reducer

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,14 +1,23 @@
 // import history to enable router navigation
 import { useHistory } from "react-router-dom";
 
+// import state to enable inputValue rendering
+import { useState } from "react";
+
+// import dispatch to enable reducer dispatch
+import { useDispatch } from "react-redux";
+
 function Form({ formType }) {
   // history constant
   const history = useHistory();
+  // dispatch constant
+  const dispatch = useDispatch();
 
   // variables to customize form text and type
   let headerText = "";
   let labelText = "";
   let inputType = "number";
+  let dispatchType = "";
   let nextPath = "";
   let requiredBool = true;
 
@@ -17,33 +26,44 @@ function Form({ formType }) {
     case "feeling":
       headerText = "How are you feeling today?";
       labelText = "Feeling (1-5):";
+      dispatchType = "SET_FEELING";
       nextPath = "/understanding";
       break;
     case "understanding":
       headerText = "How well are you understanding the content?";
       labelText = "Understanding (1-5):";
+      dispatchType = "SET_UNDERSTANDING";
       nextPath = "/support";
       break;
     case "support":
       headerText = "How well are you feeling supported in your learning?";
       labelText = "Support (1-5):";
+      dispatchType = "SET_SUPPORT";
       nextPath = "/comments";
       break;
     case "comments":
       headerText = "Any additional comments you'd like to add?";
       labelText = "Comments:";
+      dispatchType = "SET_COMMENTS";
       nextPath = "/review";
       inputType = "text";
       requiredBool = false;
       break;
   }
 
+  // form input value
+  const [inputValue, setInputValue] = useState("");
+
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    //TODO: save input data to reducer
-
-    history.push(nextPath);
+    // if there is a value in the input field and it is required for this page,
+    // store data in reducer and move to the next page
+    if (inputValue || !requiredBool) {
+      //TODO: save input data to reducer
+      dispatch({ type: dispatchType, payload: inputValue });
+      history.push(nextPath);
+    }
   };
 
   return (
@@ -51,7 +71,14 @@ function Form({ formType }) {
       <h3>{headerText}</h3>
       <form onSubmit={handleSubmit}>
         <label htmlFor="rating-input">{labelText}</label>
-        <input type={inputType} min="1" max="5" name="rating-input" />
+        <input
+          type={inputType}
+          onChange={(e) => setInputValue(e.target.value)}
+          value={inputValue}
+          min="1"
+          max="5"
+          name="rating-input"
+        />
         <button type="submit">NEXT</button>
       </form>
     </>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./components/App/App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// import redux
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import logger from "redux-logger";
+
+// create form reducer
+const formData = (state = {}, action) => {
+  switch (action.type) {
+    case "SET_FEELING":
+      return { ...state, feeling: action.payload };
+    case "SET_UNDERSTANDING":
+      return { ...state, understanding: action.payload };
+    case "SET_SUPPORT":
+      return { ...state, support: action.payload };
+    case "SET_COMMENTS":
+      return { ...state, comments: action.payload };
+  }
+  return state;
+};
+
+// create store
+const store = createStore(
+  combineReducers({ formData }),
+  applyMiddleware(logger)
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
